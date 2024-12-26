8fb75d5fc01f8e457bc9c836b72f6f94d89e4062
Sora Kanosue <sorakanosue@berkeley.edu>
2024-12-25 16:49:25 -0800

    Day1 done

--- lib/aoc2024/day1.ml
+++ lib/aoc2024/day1.ml
@@ -1,7 +1,49 @@
-let parse : string list -> int list list =
+let parse : string list -> (int * int) list =
  fun lst ->
-  List.map (fun s -> List.map int_of_string (String.split_on_char ' ' s)) lst
+  let nums =
+    List.map
+      (fun s -> List.map int_of_string (Str.split (Str.regexp {| +|}) s))
+      lst
+  in
+  List.map
+    (fun n ->
+      match n with
+      | [n1; n2] ->
+          (n1, n2)
+      | _ ->
+          raise (Invalid_argument "Unexpected number format") )
+    nums
 
-let solve_part_1 : string list -> string = fun lst -> List.hd lst
+let solve_part_1 : string list -> string =
+ fun lst ->
+  let lst = parse lst in
+  let nums1, nums2 = List.split lst in
+  let nums1 = List.sort ( - ) nums1 in
+  let nums2 = List.sort ( - ) nums2 in
+  string_of_int
+    (List.fold_left2 (fun acc a b -> acc + Int.abs (a - b)) 0 nums1 nums2)
 
-let solve_part_2 : string list -> string = fun s -> List.hd s
+module IntMap = Map.Make (Int)
+
+let solve_part_2 : string list -> string =
+ fun lst ->
+  let lst = parse lst in
+  let nums1, nums2 = List.split lst in
+  let counts = IntMap.empty in
+  let counts =
+    List.fold_left
+      (fun counts n ->
+        match IntMap.find_opt n counts with
+        | Some n' ->
+            IntMap.add n (n' + 1) counts
+        | None ->
+            IntMap.add n 1 counts )
+      counts nums2
+  in
+  let muls =
+    List.map
+      (fun n ->
+        match IntMap.find_opt n counts with Some n' -> n * n' | None -> 0 )
+      nums1
+  in
+  string_of_int (List.fold_left ( + ) 0 muls)
